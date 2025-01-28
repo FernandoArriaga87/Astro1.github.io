d1bf9e4bc9d1c26bb198b87747dd0904243c4292
Fernando <cbm0100687arriaga@gmail.com>
2025-01-27 21:08:53 -0600

    Configuración de Tailwind y Flowbite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // Importa la integración de Tailwind
 
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://fernandoarriaga87.github.io/Astro1.github.io',
-  outDir: './docs',
-  integrations: [tailwind()]
+  integrations: [tailwind()], // Integra Tailwind CSS
+  site: 'https://fernandoarriaga87.github.io/Astro1.github.io/', // Configura la URL base
+  outDir: './docs', // Carpeta de salida para los archivos de producción
+  base: '/Astro1.github.io/', // Ruta base (si tu repositorio no está en la raíz)
 });
